dbe20e42b6e6d1dce136860ab36ab2fac6a088b3
Ronny kisbye <111735396+Ronnykisbye@users.noreply.github.com>
2026-08-26 10:38:59 +0200

    Opdater cache efter udrejsehotel

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rejser-v1-20260826-1';
+const CACHE='rejser-v1-20260826-2';
 const CORE=[
   './','./index.html','./hub.css','./hub.js','./manifest.webmanifest','./data/trips.json',
   './antwerpen.html','./antwerpen.css','./antwerpen.js','./data/antwerpen.json',
