6e518aa4e3d25b1f027c82218e0e75197f932599
Ronny kisbye <111735396+Ronnykisbye@users.noreply.github.com>
2026-08-27 10:57:27 +0200

    Opdater cache efter pistekort

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rejser-v1-20260827-10';
+const CACHE='rejser-v1-20260827-11';
 const CORE=[
   './','./index.html','./hub.css','./hub.js','./manifest.webmanifest','./data/trips.json',
   './antwerpen.html','./antwerpen.css','./antwerpen.js','./data/antwerpen.json',
