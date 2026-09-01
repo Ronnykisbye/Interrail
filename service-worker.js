25577419fddf99438e0bd0a40748d3488348522d
Ronny kisbye <111735396+Ronnykisbye@users.noreply.github.com>
2026-09-01 08:46:22 +0200

    Opdater cache efter LEZ-godkendelse

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rejser-v1-20260827-12';
+const CACHE='rejser-v1-20260901-1';
 const CORE=[
   './','./index.html','./hub.css','./hub.js','./manifest.webmanifest','./data/trips.json',
   './antwerpen.html','./antwerpen.css','./antwerpen.js','./data/antwerpen.json',
