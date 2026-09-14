09429b180c8d4b10ebb99072b759875206dd038b
Ronny kisbye <111735396+Ronnykisbye@users.noreply.github.com>
2026-09-14 21:12:56 +0200

    Opdater cache efter udvidet Athen-guide

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rejser-v1-20260914-7';
+const CACHE='rejser-v1-20260914-8';
 const CORE=[
   './','./index.html','./hub.css','./hub.js','./manifest.webmanifest','./data/trips.json',
   './antwerpen.html','./antwerpen.css','./antwerpen.js','./data/antwerpen.json',
